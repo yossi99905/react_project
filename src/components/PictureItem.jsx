12137bf4085034b7edb8424d02ca0129c78fd306
yossi mizrachi <yossimizrach.770@gmail.com>
2024-02-21 10:03:40 +0200

    your commit message

--- src/components/PictureItem.jsx
+++ src/components/PictureItem.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const PictureItem = (props) => {
   return (
-    <div className='border border-success-subtle border-3 col-md-3 m-4 pt-3 itemShope shadow bg-white'>
+    <div className='border border-success-subtle border-3 col-lg-3 m-4 pt-3 itemShope shadow bg-white'>
         <h3 className='fs-4'>{props.name}</h3>
         <img className='my-3 shadow' src={props.src} width={200} height={100} alt={props.name} />
         <div>price: {props.price}$</div>
